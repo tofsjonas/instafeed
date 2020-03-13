26a48eb1f9a370ed8ef68ae6c1cc5afdd759ca07
Jonas Earendel <tofsjonas@users.noreply.github.com>
2020-03-13 17:18:18 +0100

    font-weight normal

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,17 +1,13 @@
 declare var window: any
 declare var document: any
 import { instafeed } from './feed/instafeed'
-
-// console.log('TOFS-TAG: index.ts', window['instafeed'])
 if (!window['jInstafeed']) {
-  // console.log('TOFS-TAG: index.ts', 'jonas')
   window['jInstafeed'] = instafeed
   window['jinstafunx'] = Object()
   var css_link = document.createElement('link')
   css_link.rel = 'stylesheet'
   css_link.href = document['currentScript']['src'].replace(/(\.min)*\.js$/, '$1.css')
   document['head'].appendChild(css_link)
-
 }
 
 
@@ -27,20 +23,3 @@ if (!window['jInstafeed']) {
  *      pros: better ux?
  *      cons: more code
  */
-
-// not supported by ie11 :(
-// https://medium.com/@zackcreach/fighting-instagrams-api-in-2017-832e8f0280ee
-// console.log('Starting instagram..')
-
-// const fetchData = (url: string) => {
-//   return fetch(url)
-//     .then(data => data.json())
-//     .then(json => {
-//       if (json) {
-//         return Promise.resolve(json)
-//       } else {
-//         return Promise.reject(Error('json is undefined!'))
-//       }
-//     })
-// }
-
